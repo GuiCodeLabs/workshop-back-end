fcd8b2846e28fe8a819f635ea08cb4fddcea5b8c
Guilherme <guilhermebeserra2007@gmail.com>
2026-03-19 17:37:18 -0300

    CRUD Melhorado

--- meuapp/models.py
+++ meuapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=30)
     idade = models.IntegerField()
     email = models.EmailField(unique=True)
     telefone = models.CharField(max_length=15)
-    
+
     def __str__(self):
         return self.nome
 
